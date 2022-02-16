0112b0081f8e70367eb3a4a94db34079de730add
Bhargav Joshi <bhargavjoshi@Bhargavs-MacBook-Pro.local>
2022-02-15 19:08:36 -0600

    FrontEnd: Code formatting

--- Django-App/leadmanager/frontend/src/components/layout/Header.js
+++ Django-App/leadmanager/frontend/src/components/layout/Header.js
@@ -1,22 +1,31 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class Header extends Component {
   render() {
     return (
-    <nav className="navbar navbar-expand-sm navbar-light bg-light">
+      <nav className="navbar navbar-expand-sm navbar-light bg-light">
         <div className="container-fluid">
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarTogglerDemo01"
+            aria-controls="navbarTogglerDemo01"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
             <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-                <a className="navbar-brand" href="#">ML-WebApp</a>
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                </ul>
-            </div>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <a className="navbar-brand" href="#">
+              ML-WebApp React
+            </a>
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0"></ul>
+          </div>
         </div>
-    </nav>
-    )
+      </nav>
+    );
   }
 }
 
-export default Header
+export default Header;
