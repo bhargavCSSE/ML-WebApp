df50ce406f65c31afc94309226ee47389cfbf35f
Bhargav Joshi <bhargavjoshi@Bhargavs-MacBook-Pro.local>
2022-02-15 18:30:09 -0600

    FrontEnd: Template changed to solaris

--- Django-App/leadmanager/frontend/src/components/layout/Header.js
+++ Django-App/leadmanager/frontend/src/components/layout/Header.js
@@ -9,7 +9,7 @@ export class Header extends Component {
             <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-                <a className="navbar-brand" href="#">Lead Manager</a>
+                <a className="navbar-brand" href="#">ML-WebApp</a>
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 </ul>
             </div>
